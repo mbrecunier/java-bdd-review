95d5c474bcb9d5b8b0c8235d16a591c6b7f93720
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-02-12 09:15:56 -0800

    test for host loading to home page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,13 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
   public static String vowelReplacer(String originalString) {
@@ -18,7 +24,6 @@ public class App {
         puzzleString = puzzleString.replaceAll(vowel, "-");
       }
     }
-
     return puzzleString;
   }
 }
